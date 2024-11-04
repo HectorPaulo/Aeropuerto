a884ce5bfbccfe55b8306331a049c97be8e5936a
Hector Paulo <adrian.paulo.vaz@gmail.com>
2024-11-04 16:08:28 -0600

    directorios

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
