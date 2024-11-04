fb674096a298b33a834f89334158e90eeefb2d7b
UzielLH <014430576@ulsaoaxaca.edu.mx>
2024-11-04 15:47:47 -0600

    commit

---
+++ src/services/TripulacionService.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const API_URL = "https://aeropuertolasalle-f1af9eaa4f6d.herokuapp.com/";
+
+const TripulacionService={
+    createPiloto:async (piloto) => {
+        try {
+            const response = await axios.post(`${API_URL}/create/piloto`, piloto);
+            return response.data;
+          } catch (error) {
+            console.error('Error creating piloto:', error);
+            throw error;
+          }
+    },
+    createCopiloto:async (copiloto) => {
+        try {
+            const response = await axios.post(`${API_URL}/create/copiloto`, copiloto);
+            return response.data;
+          } catch (error) {
+            console.error('Error creating copiloto:', error);
+            throw error;
+          }
+    },
+    createSobrecargo:async (sobrecargo) => {
+        try {
+            const response = await axios.post(`${API_URL}/create/sobrecargo`, sobrecargo);
+            return response.data;
+          } catch (error) {
+            console.error('Error creating sobrecargo:', error);
+            throw error;
+          }
+    },
+    mostrarTripulacion:async () => {
+        try {
+            const response = await axios.get(`${API_URL }/all`);
+            return response.data;
+          } catch (error) {
+            console.error('Error fetching clientes:', error);
+            throw error;
+          }
+    }
+};
+export default TripulacionService;
+
